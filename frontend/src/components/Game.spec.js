857c43596f3698e8dc15b226877781e3f689828d
Max Carey <mh.carey@pm.me>
2020-06-08 07:13:27 -0500

    Remove confusing describe.each
    I was using describe.each to cover browserSupportsSpeechRecognition
    true and false, but since the tests are completely different for each
    case, this made the code more complex than it needed to be. Changed to
    use two separate describe blocks.

--- frontend/src/components/Game.spec.js
+++ frontend/src/components/Game.spec.js
@@ -4,9 +4,41 @@ import { expect } from 'chai';
 
 describe('Game component', function() {
 
-    const nonsupportedBrowserTests = () => {
-        it('should show an error message',
-            function() {
+    beforeEach(() => {
+        /**
+         * Reset modules so we can mock the SpeechRecognition HOC differently in
+         * different tests. For example, the property
+         * browserSupportsSpeechRecognition is normally set once when the Game
+         * module is imported, but we want to run tests that emulate both
+         * supported and non-supported browsers.
+         *
+         * See
+         *
+         *     https://github.com/facebook/jest/issues/2582#issuecomment-378677440
+         *
+         *  and
+         *
+         *     https://medium.com/trabe/mocking-different-values-for-the-same-module-using-jest-a7b8d358d78b
+         */
+        jest.resetModules()
+    });
+
+    describe('browserSupportsSpeechRecognition: false', function() {
+
+        it('should show an error message', function() {
+            jest.mock('react-speech-recognition', () => {
+                return options => {
+                    return component => {
+                        component.defaultProps = {
+                            ...component.defaultProps,
+                            browserSupportsSpeechRecognition: false,
+                            recognition: {}
+                        };
+                        return component;
+                    };
+                };
+            });
+
             const Game = require('./Game').default;
             const wrapper = shallow(<Game />);
 
@@ -14,10 +46,28 @@ describe('Game component', function() {
             expect(wrapper.find('.error'))
                 .to.have.text("Your browser doesn't support speech recognition");
         });
-    };
 
-    const supportedBrowserTests = (mockProps) => {
+    });
+
+    describe('browserSupportsSpeechRecognition: true', function() {
+
         it('should call startListening on button click', function() {
+            const mockStartListening = jest.fn();
+
+            jest.mock('react-speech-recognition', () => {
+                return options => {
+                    return component => {
+                        component.defaultProps = {
+                            ...component.defaultProps,
+                            browserSupportsSpeechRecognition: true,
+                            recognition: {},
+                            startListening: mockStartListening
+                        };
+                        return component;
+                    };
+                };
+            });
+
             const Game = require('./Game').default;
             const wrapper = shallow(<Game />);
 
@@ -25,53 +75,9 @@ describe('Game component', function() {
 
             wrapper.find('button').simulate('click');
 
-            expect(mockProps.startListening).to.have.beenCalledTimes(1);
+            expect(mockStartListening).to.have.beenCalledTimes(1);
         });
-    };
-
-    describe.each([
-        [false, {}, nonsupportedBrowserTests],
-        [true, { startListening: jest.fn() }, supportedBrowserTests]
-    ])(
-        'browser supports speech recognition: %j',
-        (mockBrowserSupportsSpeechRecognition, mockAdditionalProps, runTests) => {
-
-        beforeEach(() => {
-            /**
-             * Mock the SpeechRecognition HOC so we can set different values
-             * for properties in different tests. For example, the property
-             * browserSupportsSpeechRecognition is normally set once when the
-             * Game module is imported, but we want to run tests that emulate
-             * both supported and non-supported browsers.
-             *
-             * See
-             *
-             *     https://github.com/facebook/jest/issues/2582#issuecomment-378677440
-             *
-             *  and
-             *
-             *     https://medium.com/trabe/mocking-different-values-for-the-same-module-using-jest-a7b8d358d78b
-             */
-            jest.resetModules()
-            jest.mock('react-speech-recognition', () => {
-                return options => {
-                    return component => {
-                        component.defaultProps = {
-                            ...component.defaultProps,
-                            ...mockAdditionalProps,
-                            browserSupportsSpeechRecognition:
-                                mockBrowserSupportsSpeechRecognition,
-                            recognition: {}
-                        };
-                        return component;
-                    };
-                };
-            });
-        });
-
-        runTests(mockAdditionalProps);
 
     });
-
 
 });
