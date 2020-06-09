c9736722cb99cb42570b015fae7fd2a5883fb6f7
Max Carey <mh.carey@pm.me>
2020-06-09 10:26:10 -0500

    Add function to convert text to SAN
    Since Google Chrome's implementation of the SpeechRecognition API
    doesn't yet support custom grammars, it doesn't recognize many common
    chess terms (e.g. it transcribes "rook" as "Brooke"). It also can't
    transcribe spoken words directly into chess algebraic notation (e.g.
    "knight to e3" -> "Ne3"). Add a function that converts the output from
    Chrome's speech-to-text API to chess standard algebraic notation (SAN).

---
+++ frontend/src/text-to-san.spec.js
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+
+import textToSan from './text-to-san';
+
+describe('textToSan', function() {
+
+    it.each([
+        ['bishop to D7', 'Bd7'],
+        ['rook A1', 'Ra1'],
+        ['queen captures H8', 'Qxh8'],
+        ['king takes F5', 'Kxf5'],
+        ['knight a to B4', 'Nab4'],
+        ['Bishop 2 h8', 'B2h8'],
+        ['Queen C2D3', 'Qc2d3'],
+        ['F captures G4 en passant', 'fxg3'],
+        ['a takes b5 en passant', 'axb4'],
+        ['E5', 'e5'],
+        ['h take G6', 'hxg6'],
+        ['c8 promote to Queen', 'c8=Q'],
+        ['F captures E8 promote to knight', 'fxe8=N'],
+        ['rook takes b7 mate', 'Rxb7#'],
+        ['Bishop A c3 check', 'Bac3+'],
+        ['E7 check', 'e7+'],
+        ['castle kingside', 'O-O'],
+        ['castle Queenside', 'O-O-O'],
+        ['Black Resigns', '1-0'],
+        ['white resigns', '0-1'],
+    ])('textToSan(%j)', (text, expected) => {
+        expect(textToSan(text)).to.equal(expected);
+    });
+
+    it('should throw an error on invalid en passant', function() {
+        expect(() => textToSan('g takes h7 en passant'))
+            .to.throw('Invalid en passant capture');
+    });
+
+});
