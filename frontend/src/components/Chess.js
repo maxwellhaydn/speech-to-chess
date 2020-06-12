f2ccbae723851d9dac0cfd191ae51113e880dd3f
Max Carey <mh.carey@pm.me>
2020-06-12 10:53:27 -0500

    Pass chess API ref to Chess component as a prop

--- frontend/src/components/Chess.js
+++ frontend/src/components/Chess.js
@@ -1,18 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Chess as ChessJS } from 'chess.js';
 
 import MoveHistoryTable from './MoveHistoryTable';
 
-const propTypes = {};
+const propTypes = {
+    apiRef: PropTypes.shape({
+        current: PropTypes.any
+    }).isRequired
+};
 
 /**
  * A chess game. Wraps the chess.js library to get things like move validation.
  */
-const Chess = (props) => {
-    const game = useRef(new ChessJS());
+const Chess = ({ apiRef }) => {
+    useEffect(() => {
+        apiRef.current = new ChessJS();
+    }, []);
 
-    return <MoveHistoryTable moves={game.current.history()} />;
+    return (
+        <MoveHistoryTable
+            moves={apiRef.current && apiRef.current.history() || []}
+        />
+    );
 };
 
 Chess.propTypes = propTypes;
--- frontend/src/components/Chess.spec.js
+++ frontend/src/components/Chess.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { expect } from 'chai';
 
 import Chess from './Chess';
@@ -8,7 +8,7 @@ import MoveHistoryTable from './MoveHistoryTable';
 describe('Chess component', function() {
 
     it('should show the move history', function() {
-        const wrapper = shallow(<Chess />);
+        const wrapper = mount(<Chess apiRef={{}} />);
 
         expect(wrapper).to.contain(<MoveHistoryTable moves={[]} />);
     });
