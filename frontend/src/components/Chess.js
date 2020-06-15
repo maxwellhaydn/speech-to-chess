09177d0a98798677998262ff04fd86d48d8d54b6
Max Carey <mh.carey@pm.me>
2020-06-15 07:04:10 -0500

    Conditionally import chess.js
    chess.js exports an object
    
        { Chess: Chess }
    
    on CommonJS environments but simply exports the Chess constructor on
    AMD environments (https://github.com/jhlywa/chess.js/issues/196). This
    causes a compilation error in either the browser or in Node if you run
    the same code in both. Which one fails depends on how you do the import.
    This commit does the import conditionally depending on the environment.

--- frontend/src/components/Chess.js
+++ frontend/src/components/Chess.js
@@ -1,8 +1,27 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Chess as ChessJS } from 'chess.js';
+import { isNode } from 'browser-or-node';
 
 import MoveHistoryTable from './MoveHistoryTable';
+
+let ChessJS;
+
+/**
+ * In CommonJS environments, chess.js exports an object with the Chess
+ * constructor as a property, while on AMD environments it exports the Chess
+ * constructor directly. Webpack supports both CommonJS and AMD. Due to an issue
+ * in chess.js (https://github.com/jhlywa/chess.js/issues/196), you get the AMD
+ * export with Webpack, so tests running in Node and code bundled with Webpack
+ * cannot both use the same import signature. The following is a temporary
+ * workaround until the issue in chess.js is resolved.
+ */
+if (isNode) {
+    const chess = require('chess.js');
+    ChessJS = chess.Chess;
+}
+else {
+    ChessJS = require('chess.js');
+}
 
 const propTypes = {
     apiRef: PropTypes.shape({
