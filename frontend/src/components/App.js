55701c93943bfb5e9de3e775febebda12fdea531
Max Carey <mh.carey@pm.me>
2020-06-15 11:19:40 -0500

    Move all chess logic into App component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,86 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import ChessNLP from 'chess-nlp';
+import { isNode } from 'browser-or-node';
 
-import Chess from './Chess';
+import GameStatusIndicator from './GameStatusIndicator';
+import MoveHistoryTable from './MoveHistoryTable';
 import VoiceCommand from './VoiceCommand';
 
+let Chess;
+
+/**
+ * In CommonJS environments, chess.js exports an object with the Chess
+ * constructor as a property, while on AMD environments it exports the Chess
+ * constructor directly. Webpack supports both CommonJS and AMD. Due to an issue
+ * in chess.js (https://github.com/jhlywa/chess.js/issues/196), you get the AMD
+ * export with Webpack, so tests running in Node and code bundled with Webpack
+ * cannot both use the same import signature. The following is a temporary
+ * workaround until the issue in chess.js is resolved.
+ */
+if (isNode) {
+    const chess = require('chess.js');
+    Chess = chess.Chess;
+}
+else {
+    Chess = require('chess.js');
+}
+
+const parserOptions = {
+    aliases: {
+        knight: ['night']
+    }
+};
+
+const parser = new ChessNLP(parserOptions);
+
+/**
+ * A chess game played with voice commands.
+ */
 const App = (props) => {
-    const chessAPIRef = useRef(null);
+    const chessApiRef = useRef(new Chess());
+    const [moves, setMoves] = useState([]);
+
+    const turn = useCallback(() => {
+        const player = chessApiRef.current.turn() === 'b' ? 'Black' : 'White';
+        return `${player} to move`;
+    }, [chessApiRef]);
+
+    const [status, setStatus] = useState({ message: turn() });
+
+    const handleVoiceCommand = useCallback((command) => {
+        let move;
+
+        try {
+            move = parser.toSAN(command);
+        }
+        catch (error) {
+            setStatus({ message: 'Invalid move', details: command });
+            return;
+        }
+
+        if (chessApiRef.current.move(move)) {
+            setMoves(chessApiRef.current.history());
+
+            if (chessApiRef.current.game_over()) {
+                setStatus({ message: 'Game over' });
+            }
+            else {
+                setStatus({ message: turn() });
+            }
+        }
+        else {
+            setStatus({ message: 'Invalid move', details: move });
+        }
+    }, [chessApiRef, setMoves, setStatus, turn]);
 
     return (
         <div className="app">
-            <VoiceCommand />
-            <Chess apiRef={chessAPIRef} />
+            <VoiceCommand onCommand={handleVoiceCommand} />
+            <MoveHistoryTable moves={moves} />
+            <GameStatusIndicator
+                status={status.message}
+                details={status.details}
+            />
         </div>
     );
 };
--- frontend/src/components/App.spec.js
+++ frontend/src/components/App.spec.js
@@ -1,20 +1,112 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
+import { Chess } from 'chess.js';
+import ChessNLP from 'chess-nlp';
 
 import App from './App';
-import Chess from './Chess';
+import GameStatusIndicator from './GameStatusIndicator';
+import MoveHistoryTable from './MoveHistoryTable';
 import VoiceCommand from './VoiceCommand';
+
+jest.mock('chess.js', () => ({
+    Chess: jest.fn().mockImplementation(() => ({
+        move: move => true,
+        turn: () => 'w',
+        game_over: () => false,
+        history: () => []
+    }))
+}));
+
+jest.mock('chess-nlp', () => ({
+    __esModule: true,
+    default: jest.fn().mockImplementation((options) => ({
+        toSAN: text => text
+    }))
+}));
 
 describe('App component', function() {
 
-    it('should contain a VoiceCommand and a Chess component', function() {
+    beforeEach(function() {
+        jest.restoreAllMocks();
+        jest.clearAllMocks();
+    });
+
+    it('should show an empty moves table with white to move on initial render', function() {
         const wrapper = shallow(<App />);
 
         expect(wrapper).to.containMatchingElement(<VoiceCommand />);
-        expect(wrapper).to.containMatchingElement(
-            <Chess apiRef={{ current: null }} />
+        expect(wrapper).to.contain(<MoveHistoryTable moves={[]} />);
+        expect(wrapper).to.contain(
+            <GameStatusIndicator status="White to move" />
         );
+    });
+
+    it('should make a move when a valid voice command is given', function() {
+        Chess.mockImplementation(() => ({
+            move: move => true,
+            turn: () => 'b',
+            game_over: () => false,
+            history: () => ['e4']
+        }));
+
+        const wrapper = shallow(<App />);
+
+        wrapper.find(VoiceCommand).invoke('onCommand')('e4');
+
+        expect(wrapper).to.contain(<MoveHistoryTable moves={['e4']} />);
+        expect(wrapper).to.contain(
+            <GameStatusIndicator status="Black to move" />
+        );
+    });
+
+    it("should show an error message when voice command can't be parsed", function() {
+        ChessNLP.mockImplementation(() => ({
+            toSAN: text => { throw new Error(); }
+        }));
+
+        const wrapper = shallow(<App />);
+
+        wrapper.find(VoiceCommand).invoke('onCommand')('foo');
+
+        expect(wrapper).to.contain(<MoveHistoryTable moves={[]} />);
+        expect(wrapper).to.contain(
+            <GameStatusIndicator status="Invalid move" details="foo" />
+        );
+    });
+
+    it('should show an error message when move is invalid', function() {
+        Chess.mockImplementation(() => ({
+            move: move => null,
+            turn: () => 'w',
+            game_over: () => false,
+            history: () => []
+        }));
+
+        const wrapper = shallow(<App />);
+
+        wrapper.find(VoiceCommand).invoke('onCommand')('Ke8');
+
+        expect(wrapper).to.contain(<MoveHistoryTable moves={[]} />);
+        expect(wrapper).to.contain(
+            <GameStatusIndicator status="Invalid move" details="Ke8" />
+        );
+    });
+
+    it('should show a message when the game is over', function() {
+        Chess.mockImplementation(() => ({
+            move: move => true,
+            turn: () => 'b',
+            game_over: () => true,
+            history: () => ['e4']
+        }));
+
+        const wrapper = shallow(<App />);
+
+        wrapper.find(VoiceCommand).invoke('onCommand')('e4');
+
+        expect(wrapper).to.contain(<MoveHistoryTable moves={['e4']} />);
+        expect(wrapper).to.contain(<GameStatusIndicator status="Game over" />);
     });
 
 });
